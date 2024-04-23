82198db497335cb9f9ab25c4ec66dedcbd175da1
Kris Stasik <kstasik@gmail.com>
2024-04-23 19:44:06 -0400

    inital dap integration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,9 @@
+-- TODO:
+-- - dap
+-- - dap with cmake-tools
+-- - dap with neotest
+-- - cmake-tools with lualine instead of nvim-notify
+
 -- disable netrw, use nvim-tree instead
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
@@ -85,7 +91,6 @@ require("lazy").setup(
         "L3MON4D3/LuaSnip", -- core nvim snippet engine
         "hrsh7th/nvim-cmp", -- interface for completion sources
         "saadparwaiz1/cmp_luasnip", -- nvim-cmp integration for LuaSnip
-        "hrsh7th/cmp-buffer", -- nvim-cmp source for text in buffer
         "hrsh7th/cmp-nvim-lsp", -- nvim-cmp source for lsp
         --"hrsh7th/cmp-path",
         --"hrsh7th/cmp-cmdline",
@@ -113,12 +118,16 @@ require("lazy").setup(
       "nvim-lualine/lualine.nvim",
       dependencies = { "nvim-tree/nvim-web-devicons" }
     },
+    { "mfussenegger/nvim-dap" },
+    { "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} },
   }
 )
 
 require("neotest").setup({
   adapters = {
     require("neotest-gtest").setup({
+      debug_adapter = 'lldb',
+      -- exclude external and intermediate directories
       filter_dir = function(name, rel_path, root)
         return name ~= "extern" and name ~= "build"
       end,
@@ -141,12 +150,18 @@ cmp.setup({
   },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'buffer' },
   }),
   snippet = { -- configure how nvim-cmp interacts with snippet engine
 	  expand = function(args)
       luasnip.lsp_expand(args.body)
 	  end
+  },
+  matching = { -- disable fuzzy matching for completion
+    disallow_fuzzy_matching = true,
+    disallow_fullfuzzy_matching = true,
+    disallow_partial_fuzzy_matching = true,
+    disallow_partial_matching = true,
+    disallow_prefix_unmatching = false,
   },
   mapping = cmp.mapping.preset.insert({
     ['<cr>'] = cmp.mapping.confirm({select = true}),
@@ -157,7 +172,7 @@ cmp.setup({
   }),
 })
 
-require'lspconfig'.clangd.setup({
+require('lspconfig').clangd.setup({
   capabilities = require('cmp_nvim_lsp').default_capabilities(),
   on_attach = function(client, bufnr)
     -- lsp
@@ -179,9 +194,79 @@ require('lspconfig').tsserver.setup({
 
 require("cmake-tools").setup({
   cmake_build_directory = "build/${variant:buildType}",
+  cmake_dap_configuration = {
+    name = 'cpp',
+    type = 'lldb',
+    request = 'launch',
+    stopOnEntry = false,
+    runInTerminal = true,
+    console = 'integratedTerminal',
+  },
 })
 
 require("lualine").setup()
+
+local dap = require("dap")
+local dapui = require("dapui")
+dap.adapters.lldb = {
+  type = 'executable',
+  command = '/opt/homebrew/bin/lldb-dap',
+  name = 'lldb',
+}
+
+dap.configurations.cpp = {
+  {
+    name = 'Launch',
+    type = 'lldb',
+    request = 'launch',
+    program = function() 
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = true,
+    args = {},
+    -- runInTerminal = true,
+    env = function()
+      local variables = {}
+      for k, v in pairs(vim.fn.environ()) do
+        table.insert(variables, string.format("%s=%s", k, v))
+      end
+      return variables
+    end,
+  },
+  {
+    -- If you get an "Operation not permitted" error using this, try disabling YAMA:
+    --  echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+    name = "Attach to process",
+    type = 'lldb',
+    request = 'attach',
+    pid = require('dap.utils').pick_process,
+    args = {},
+    env = function()
+      local variables = {}
+      for k, v in pairs(vim.fn.environ()) do
+        table.insert(variables, string.format("%s=%s", k, v))
+      end
+      return variables
+    end,
+    },
+}
+dap.configurations.c = dap.configurations.cpp
+
+dapui.setup()
+
+dap.listeners.before.attach.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+  dapui.close()
+end
 
 vim.cmd.colorscheme('rose-pine-moon')
 
@@ -210,3 +295,32 @@ vim.keymap.set('n', '<leader>cg', ':CMakeGenerate<cr>')
 vim.keymap.set('n', '<leader>cG', ':CMakeGenerate!<cr>')
 vim.keymap.set('n', '<leader>cr', ':CMakeRun<cr>')
 vim.keymap.set('n', '<leader>ct', ':CMakeSelectBuildType<cr>')
+
+-- dap
+vim.keymap.set('n', '<leader>dr', ':DapContinue<cr>') -- r
+vim.keymap.set('n', '<leader>dc', ':DapContinue<cr>') -- c
+vim.keymap.set('n', '<leader>dn', ':DapStepOver<cr>') -- n, ni
+vim.keymap.set('n', '<leader>ds', ':DapStepInto<cr>') -- s, si
+vim.keymap.set('n', '<leader>do', ':DapStepOut<cr>') -- thread step-out
+vim.keymap.set('n', '<leader>db', ':DapToggleBreakpoint<cr>') -- b, br
+
+vim.keymap.set('n', '<Meta-n>', ':DapStepOver<cr>') -- n, ni
+vim.keymap.set('n', '<Meta-i>', ':DapStepInto<cr>') -- s, si
+vim.keymap.set('n', '<Meta-o>', ':DapStepOut<cr>') -- thread step-out
+
+-- To use the bundled libc++ please add the following LDFLAGS:
+-- LDFLAGS="-L/opt/homebrew/opt/llvm/lib/c++ -Wl,-rpath,/opt/homebrew/opt/llvm/lib/c++"
+
+-- llvm is keg-only, which means it was not symlinked into /opt/homebrew,
+-- because macOS already provides this software and installing another version in
+-- parallel can cause all kinds of trouble.
+
+-- If you need to have llvm first in your PATH, run:
+-- echo 'export PATH="/opt/homebrew/opt/llvm/bin:$PATH"' >> ~/.zshrc
+
+-- For compilers to find llvm you may need to set:
+-- export LDFLAGS="-L/opt/homebrew/opt/llvm/lib"
+-- export CPPFLAGS="-I/opt/homebrew/opt/llvm/include"
+--
+-- To use lldp-dap only run:
+-- ln -s $(brew --prefix)/opt/llvm/bin/lldb-dap $(brew --prefix)/bin/
---
+++ .zshrc
@@ -0,0 +1,2 @@
+alias ls='ls --color=tty'
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
